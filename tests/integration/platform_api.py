fb80123ba760f3217b1759e3a108bae5a027d2bb
Ivan <vanya.zubenko@neuromation.io>
2020-07-30 15:29:38 +0300

    add api

---
+++ tests/integration/platform_api.py
@@ -0,0 +1,47 @@
+import aiohttp.web
+import pytest
+
+
+@pytest.fixture
+def platform_api_app() -> aiohttp.web.Application:
+    async def _get_config(request: aiohttp.web.Request) -> aiohttp.web.Response:
+        return aiohttp.web.json_response(
+            {
+                "headless_callback_url": "headless_callback_url",
+                "auth_url": "auth_url",
+                "token_url": "token_url",
+                "client_id": "client_id",
+                "audience": "audience",
+                "clusters": [
+                    {
+                        "name": "default",
+                        "registry_url": "registry_url",
+                        "storage_url": "storage_url",
+                        "users_url": "users_url",
+                        "monitoring_url": "monitoring_url",
+                        "secrets_url": "secrets_url",
+                        "resource_presets": [],
+                    }
+                ],
+            }
+        )
+
+    async def _get_job(request: aiohttp.web.Request) -> aiohttp.web.Response:
+        job_id = request.match_info["job_id"]
+        return aiohttp.web.json_response(
+            {
+                "id": job_id,
+                "owner": "user",
+                "cluster_name": "default",
+                "status": "running",
+                "history": {"status": "running"},
+                "container": {"image": "neuromation/base", "env": {}, "volumes": []},
+                "is_preemptible": False,
+                "uri": f"job://default/user/{job_id}",
+            }
+        )
+
+    app = aiohttp.web.Application()
+    app.router.add_get("/api/v1/config", _get_config)
+    app.router.add_get("/api/v1/jobs/{job_id}", _get_job)
+    return app
---
+++ tests/integration/platform_auth.py
@@ -0,0 +1,38 @@
+from typing import Any, Callable, Coroutine, Sequence
+
+import aiohttp
+import pytest
+from jose import jwt
+from neuro_auth_client import AuthClient, Permission, User
+from yarl import URL
+
+
+_JWT_SECRET = "secret"
+
+
+@pytest.fixture
+def token_factory() -> Callable[[str], str]:
+    def _create(name: str) -> str:
+        payload = {"https://platform.neuromation.io/user": name}
+        return jwt.encode(payload, _JWT_SECRET, algorithm="HS256")
+
+    return _create
+
+
+@pytest.fixture
+def user_factory(
+    token_factory: Callable[[str], str], platform_auth_server: URL
+) -> Callable[[str, Sequence[Permission]], Coroutine[Any, Any, None]]:
+    async def _create(name: str, permissions: Sequence[Permission] = ()) -> None:
+        async with AuthClient(
+            url=platform_auth_server, token=token_factory("admin")
+        ) as client:
+            try:
+                await client.add_user(User(name=name))
+            except aiohttp.ClientResponseError as ex:
+                if "already exists" not in ex.message:
+                    raise
+            if permissions:
+                await client.grant_user_permissions(name, permissions)
+
+    return _create

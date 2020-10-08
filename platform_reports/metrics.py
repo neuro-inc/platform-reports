b1a594c1ba916a12e3bfc80b99291066d526ab91
Ivan Zubenko <vanya.zubenko@gmail.com>
2020-10-08 19:04:46 +0300

    add aws node price collector

--- platform_reports/metrics.py
+++ platform_reports/metrics.py
@@ -1,8 +1,11 @@
 import asyncio
+import json
 import logging
 from dataclasses import dataclass
 from types import TracebackType
-from typing import Awaitable, Optional, Type
+from typing import Awaitable, Dict, Optional, Type
+
+from aiobotocore.client import AioBaseClient
 
 
 logger = logging.getLogger(__name__)
@@ -66,7 +69,73 @@ class PriceCollector:
 
 
 class AWSNodePriceCollector(PriceCollector):
-    pass
+    def __init__(
+        self,
+        ssm_client: AioBaseClient,
+        pricing_client: AioBaseClient,
+        region: str,
+        instance_type: str,
+        interval_s: float = 3600,
+    ) -> None:
+        super().__init__(interval_s)
+        self._ssm_client = ssm_client
+        self._pricing_client = pricing_client
+        self._region = region
+        self._region_long_name = ""
+        self._instance_type = instance_type
+
+    async def __aenter__(self) -> PriceCollector:
+        await super().__aenter__()
+        self._region_long_name = await self._get_region_long_name()
+        logger.info(
+            "Initialized AWS price collector for %s instance in %s region",
+            self._instance_type,
+            self._region_long_name,
+        )
+        return self
+
+    async def _get_region_long_name(self) -> str:
+        response = await self._ssm_client.get_parameter(
+            Name=f"/aws/service/global-infrastructure/regions/{self._region}/longName"
+        )
+        return response["Parameter"]["Value"]
+
+    async def get_latest_price_per_hour(self) -> Price:
+        response = await self._pricing_client.get_products(
+            ServiceCode="AmazonEC2",
+            FormatVersion="aws_v1",
+            Filters=[
+                self._create_filter("ServiceCode", "AmazonEC2"),
+                self._create_filter("locationType", "AWS Region"),
+                self._create_filter("location", self._region_long_name),
+                self._create_filter("operatingSystem", "Linux"),
+                self._create_filter("tenancy", "Shared"),
+                self._create_filter("capacitystatus", "Used"),
+                self._create_filter("preInstalledSw", "NA"),
+                self._create_filter("instanceType", self._instance_type),
+            ],
+        )
+        if len(response["PriceList"]) != 1:
+            logger.warning(
+                "AWS returned %d products in %s region, cannot determine node price",
+                len(response["PriceList"]),
+                self._region_long_name,
+            )
+            return Price()
+        price_list = json.loads(response["PriceList"][0])
+        on_demand = price_list["terms"]["OnDemand"]
+        id1 = next(iter(on_demand))
+        id2 = next(iter(on_demand[id1]["priceDimensions"]))
+        price_per_unit = on_demand[id1]["priceDimensions"][id2]["pricePerUnit"]
+        if "USD" in price_per_unit:
+            return Price(currency="USD", value=float(price_per_unit["USD"]))
+        logger.warning(
+            "AWS product currencies are not supported: %s", list(price_per_unit.keys())
+        )
+        return Price()
+
+    def _create_filter(self, field: str, value: str) -> Dict[str, str]:
+        return {"Type": "TERM_MATCH", "Field": field, "Value": value}
 
 
 class AzureNodePriceCollector(PriceCollector):
--- tests/unit/test_metrics.py
+++ tests/unit/test_metrics.py
@@ -1,11 +1,13 @@
 import asyncio
+import json
 from contextlib import suppress
 from typing import Iterator
 from unittest import mock
 
 import pytest
+from aiobotocore.client import AioBaseClient
 
-from platform_reports.metrics import Price, PriceCollector
+from platform_reports.metrics import AWSNodePriceCollector, Price, PriceCollector
 
 
 class TestPriceCollector:
@@ -34,3 +36,107 @@ class TestPriceCollector:
         task.cancel()
         with suppress(asyncio.CancelledError):
             await task
+
+
+class TestAWSNodePriceCollector:
+    @pytest.fixture
+    def ssm_client(self) -> mock.AsyncMock:
+        result = mock.AsyncMock()
+        result.get_parameter.return_value = {
+            "Parameter": {"Value": "US East (N. Virginia)"}
+        }
+        return result
+
+    @pytest.fixture
+    def pricing_client(self) -> mock.AsyncMock:
+        return mock.AsyncMock()
+
+    @pytest.fixture
+    def price_collector(
+        self, ssm_client: AioBaseClient, pricing_client: AioBaseClient
+    ) -> AWSNodePriceCollector:
+        return AWSNodePriceCollector(
+            ssm_client=ssm_client,
+            pricing_client=pricing_client,
+            region="us-east-1",
+            instance_type="p2.xlarge",
+            interval_s=0.1,
+        )
+
+    async def test_get_latest_price_per_hour(
+        self, price_collector: AWSNodePriceCollector, pricing_client: mock.AsyncMock
+    ) -> None:
+        pricing_client.get_products.return_value = {
+            "PriceList": [
+                json.dumps(
+                    {
+                        "terms": {
+                            "OnDemand": {
+                                "CGJXHFUSGE546RV6.JRTCKXETXF": {
+                                    "priceDimensions": {
+                                        "CGJXHFUSGE546RV6.JRTCKXETXF.6YS6EN2CT7": {
+                                            "pricePerUnit": {"USD": "0.1"}
+                                        }
+                                    }
+                                }
+                            }
+                        }
+                    }
+                )
+            ]
+        }
+
+        result = await price_collector.get_latest_price_per_hour()
+
+        assert result == Price(currency="USD", value=0.1)
+
+    async def test_get_latest_price_per_hour_with_multiple_prices(
+        self, price_collector: AWSNodePriceCollector, pricing_client: mock.AsyncMock
+    ) -> None:
+        price_item = {
+            "terms": {
+                "OnDemand": {
+                    "CGJXHFUSGE546RV6.JRTCKXETXF": {
+                        "priceDimensions": {
+                            "CGJXHFUSGE546RV6.JRTCKXETXF.6YS6EN2CT7": {
+                                "pricePerUnit": {"USD": "0.1"}
+                            }
+                        }
+                    }
+                }
+            }
+        }
+        pricing_client.get_products.return_value = {
+            "PriceList": [json.dumps(price_item), json.dumps(price_item)]
+        }
+
+        result = await price_collector.get_latest_price_per_hour()
+
+        assert result == Price()
+
+    async def test_get_latest_price_per_hour_with_unsupported_currency(
+        self, price_collector: AWSNodePriceCollector, pricing_client: mock.AsyncMock
+    ) -> None:
+        pricing_client.get_products.return_value = {
+            "PriceList": [
+                json.dumps(
+                    {
+                        "terms": {
+                            "OnDemand": {
+                                "CGJXHFUSGE546RV6.JRTCKXETXF": {
+                                    "priceDimensions": {
+                                        "CGJXHFUSGE546RV6.JRTCKXETXF.6YS6EN2CT7": {
+                                            "pricePerUnit": {"UAH": "0.1"}
+                                        }
+                                    }
+                                }
+                            }
+                        }
+                    }
+                )
+            ]
+        }
+
+        result = await price_collector.get_latest_price_per_hour()
+
+        assert result == Price()

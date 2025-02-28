0c4bfb2321726129b023b63e9c57854ab1b84b4e
Ivan Zubenko <vanya.zubenko@gmail.com>
2025-02-28 13:50:10 +0100

    wait for auth server to start in tests

--- tests/integration/conftest_platform_auth.py
+++ tests/integration/conftest_platform_auth.py
@@ -1,5 +1,7 @@
 from __future__ import annotations
 
+import asyncio
+import logging
 from collections.abc import Awaitable, Callable, Sequence
 from dataclasses import dataclass
 
@@ -7,10 +9,43 @@ import aiohttp
 import pytest
 from jose import jwt
 from neuro_auth_client import AuthClient, Permission, User as AuthUser
+from pytest_docker.plugin import Services
 from yarl import URL
 
 
+LOGGER = logging.getLogger(__name__)
+
+
 _JWT_SECRET = "secret"
+
+
+@pytest.fixture()
+async def platform_auth_server(docker_ip: str, docker_services: Services) -> URL:
+    port = docker_services.port_for("platform-auth", 8080)
+    url = URL(f"http://{docker_ip}:{port}")
+    await wait_for_auth_server(url)
+    return url
+
+
+async def wait_for_auth_server(
+    url: URL, timeout_s: float = 300, interval_s: float = 1
+) -> None:
+    async def _wait() -> None:
+        last_exc = None
+        try:
+            while True:
+                try:
+                    async with AuthClient(url=url, token="") as auth_client:
+                        await auth_client.ping()
+                        break
+                except (AssertionError, OSError, aiohttp.ClientError) as exc:
+                    last_exc = exc
+                LOGGER.debug("waiting for %s: %s", url, last_exc)
+                await asyncio.sleep(interval_s)
+        except asyncio.CancelledError:
+            pytest.fail(f"failed to connect to {url}: {last_exc}")
+
+    await asyncio.wait_for(_wait(), timeout=timeout_s)
 
 
 @pytest.fixture()

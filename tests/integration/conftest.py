9282051c8f03b77a3c9185fad076fed4539668d7
oleksandr-sandbx <olexandr.kostiuk@sandbx.co>
2025-04-29 19:07:28 +0200

    test

--- tests/integration/conftest.py
+++ tests/integration/conftest.py
@@ -113,7 +113,7 @@ def platform_api_config(
 
 
 @pytest.fixture
-def platform_config_config(
+async def platform_config_config(
     platform_config_server: URL, service_token: str
 ) -> PlatformServiceConfig:
     return PlatformServiceConfig(url=platform_config_server, token=service_token)
